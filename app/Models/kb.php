9e39c8c7607cf2dcd45addf2c5f7b618b77c5123
Alex Noppenberger <noppenbe@gmx.de>
2021-09-12 15:55:36 +0200

    KB

--- app/Models/kb.php
+++ app/Models/kb.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class kb extends Model
+class KB extends Model
 {
     use HasFactory;
     
